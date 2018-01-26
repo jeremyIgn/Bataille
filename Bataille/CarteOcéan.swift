18aa685b6317bed82a0a9a28afe0adb60f686d3d
Jeremy IGONIN <jeigonin@iutclinfa1009m.local>
2018-01-26 09:54:24 +0100

    Add Ship's method

--- Bataille/CarteOcéan.swift
+++ Bataille/CarteOcéan.swift
@@ -7,8 +7,56 @@
 
 import Foundation
 
-struct CarteOcean{
+
+
+
+class CarteOcean{
+    var columns:[Character]=["A","B","C","D","E"]
+    var gameBoard: [CasePosition]=[]
+    var shipList:[Ship]=[]
+
+
+func loadCasesOnMap(){
+    for eachCol in columns{
+        var hash: Int = 0
+        for eachLine in 0..<6{
+            gameBoard.append(CasePosition(hashValue: hash, line: eachLine, column: eachCol))
+            hash = hash + 1
+        }
+    }
+}
+
+func displayBoard() {    
+    for element in gameBoard{
+        print(element.column, element.line)
+    }
     
-    var carte:[CasePosition ]=[]
+}
+
+func addShipOnGameBoard(sizeShip:Int)->Bool{
+    let random:UInt32 = arc4random()
+    var indexForHashValue:UInt32 = 1 + random % 6
+    var indexForLine: UInt32 = 1 + random % 6
+    var indexForCharacter:Int = Int(1 + random % 6)
+    var col:Character = columns[indexForCharacter]
+    var casesOfNewShip:[CasePosition]=[]
+    for index in 0..<sizeShip{
+        var newCasePosition:CasePosition=CasePosition(hashValue: Int(indexForHashValue), line: Int(indexForLine), column:col)
+        casesOfNewShip.append(newCasePosition)
+        indexForCharacter+=1
+        indexForLine+=1
+        indexForHashValue+=1
+    }
+    let newShip:Ship=Ship(cases: casesOfNewShip)
+    shipList.append(newShip)
     
+    return false
+}
+func isShipEntirelyStriked(ship: Ship, gameBoard: [CasePosition: CaseState])-> Bool {
+        return false
+}
+func isGameEnded(ships: [Ship]) -> Bool {
+    return false
+}
+
 }
--- Bataille/main.swift
+++ Bataille/main.swift
@@ -10,17 +10,7 @@ import Foundation
 print("Hello, World!")
 
 var laCarteOceanMain:CarteOcean=CarteOcean()
-let columns:[Character]=["A","B","C","D","E"]
 
-
-for eachCol in columns{
-    var hash: Int = 0
-    for eachLine in 0..<5{
-        laCarteOceanMain.carte.append(CasePosition(hashValue: hash, line: eachLine, column: eachCol))
-        hash = hash + 1
-    }
-}
-
-for element in laCarteOceanMain.carte{
-    print(element.column, element.line)
-}
+laCarteOceanMain.loadCasesOnMap()
+//laCarteOceanMain.addShipOnGameBoard(sizeShip: 2)
+//laCarteOceanMain.displayBoard()
